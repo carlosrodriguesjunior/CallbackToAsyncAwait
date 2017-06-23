082edb75ab48ae76349927c8b503ddf17ec1b8dc
Carlos Rodrigues <carlosrodriguesjunior@gmail.com>
2017-06-23 20:24:28 -0300

    callback

---
+++ callback.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+app.get('/', function (req, res) {
+    MongoClient.connect('mongodb://localhost:27017/netcoders', function (err, db) {
+        if (err) throw err
+        db.collection('alunos').find().toArray(function (err, result) {
+            if (err) throw err
+            res.send(result)
+        })
+    })
+    
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
